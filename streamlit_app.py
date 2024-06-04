352dbca7b456ef01a4b4417693d4b3fc5bf593c1
phuongDoMega <pdmega268@gmail.com>
2024-06-05 01:01:19 +0700

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import ffmpeg
 import streamlit as st
 
 # global variables
-stream = ffmpeg.input('input.mp4')
+stream = ffmpeg.input('video.mp4')
 stream = ffmpeg.hflip(stream)
 stream = ffmpeg.output(stream, 'output.mp4')
 ffmpeg.run(stream)
