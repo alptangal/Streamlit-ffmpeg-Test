f0a1c2de5ec89331297c7776ea61deb06508298b
phuongDoMega <pdmega268@gmail.com>
2024-06-05 01:34:07 +0700

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,17 @@ import subprocess
 import ffmpeg
 import streamlit as st
 import os
-
-os.system('ffmpeg -i video1.mp4 -deinterlace -vcodec libx264 -pix_fmt yuv420p -preset medium -r 30 -g 60 -b:v 2500k -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k -f flv rtmp://a.rtmp.youtube.com/live2/vf7p-py39-erw8-k6z0-56d2')
+import asyncio
+import server
+import requests
+async def main():
+    try:
+        req=requests.get('http://localhost:8888')
+        print(req.status_code)
+        print('Client closed')
+        exit()
+    except:
+        os.system('ffmpeg -i video1.mp4 -deinterlace -vcodec libx264 -pix_fmt yuv420p -preset medium -r 30 -g 60 -b:v 2500k -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k -f flv rtmp://iad05.contribute.live-video.net/app/live_592628789_SEBNXuQGAcjUBxf6kaisYn4ag266WD')
 
 # global variables
 '''stream = ffmpeg.input('input.mp4')
