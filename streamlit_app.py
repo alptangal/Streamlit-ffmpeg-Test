601f819ff8defa37c98655f4f6804a6672ddd3ba
alptangal <alptangal@gmail.com>
2024-06-05 09:22:28 +0700

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ async def main():
         exit()
     except:
         server.b()
-        os.system('ffmpeg -i video1.mp4 -deinterlace -vcodec libx264 -pix_fmt yuv420p -preset medium -r 30 -g 60 -b:v 2500k -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k -f flv rtmp://iad05.contribute.live-video.net/app/live_592628789_SEBNXuQGAcjUBxf6kaisYn4ag266WD')
+        os.system('ffmpeg -stream_loop 1 -i video1.mp4 -deinterlace -vcodec libx264 -pix_fmt yuv420p -preset medium -r 30 -g 60 -b:v 2500k -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k -f flv rtmp://iad05.contribute.live-video.net/app/live_592628789_SEBNXuQGAcjUBxf6kaisYn4ag266WD')
 asyncio.run(main())
 # global variables
 '''stream = ffmpeg.input('input.mp4')
