359a3781e9e53719e9caffeba6d144242b913f9e
alptangal <alptangal@gmail.com>
2024-06-05 10:42:34 +0700

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import os
 
-os.system('''cargo install fnm
+os.system('''curl https://sh.rustup.rs -sSf | sh
+          cargo install fnm
           docker pull node:20-alpine
           npm install @dank074/discord-video-stream@latest
 npm install discord.js-selfbot-v13@latest''')
