1db59187db43c3ded4cb4bd7eaa31ffdfd4359c1
alptangal <alptangal@gmail.com>
2024-06-05 10:36:32 +0700

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import os
 
-os.system('''npm install @dank074/discord-video-stream@latest
+os.system('''curl -fsSL https://fnm.vercel.app/install | bash
+          fnm use --install-if-missing 20
+          npm install @dank074/discord-video-stream@latest
 npm install discord.js-selfbot-v13@latest''')
 os.system('node main.js')
