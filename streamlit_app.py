14f2294c9f6490351a37945c309704b077c3f53d
alptangal <alptangal@gmail.com>
2024-06-05 11:54:53 +0700

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-from nodejs import node, npm, npx
-# Run npm and return the exit code.
-npm.call(['@dank074/discord-video-stream@latest', 'discord.js-selfbot-v13@latest' ])
-node.call(['main.js'])
+import os
+os.system('node -v')
+os.system('node main.js')
