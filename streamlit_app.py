8fb5d99edcfd2c22e8ba68557b7e52fa2c5e56c6
alptangal <alptangal@gmail.com>
2024-06-05 14:51:40 +0700

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import os
-os.system('''
-          npm install @dank074/discord-video-stream@latest
-npm install discord.js-selfbot-v13@latest
-          ''')
-os.system('node main.js')
+import server
+import asyncio
+import requests
+
+async def main():
+    try:
+        req=requests.get('http://localhost:8888')
+        print(req.status_code)
+        print('Client closed')
+        exit()
+    except:
+        server.b()  
+        os.system('''
+                npm install @dank074/discord-video-stream@latest
+        npm install discord.js-selfbot-v13@latest
+                ''')
+        os.system('node main.js')
+asyncio.run(main())
