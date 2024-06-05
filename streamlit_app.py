9454ba03bb817619974b1dd507641d96137d6146
alptangal <alptangal@gmail.com>
2024-06-05 15:55:27 +0700

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,8 @@ async def main():
     try:
         req=requests.get('http://localhost:8888')
         print(req.status_code)
-        if req.status_code<400:
-            print('Client closed')
-            os._exit()
+        print('Client closed')
+        os._exit()
     except Exception as error:
         print(error)
         if 'No connection could be made because the target machine actively refused it' in str(error) or ('req' in locals() and req.status_code>=400):
