4a29a37dd0b822f8832be9a6bf79527fd6ab024b
alptangal <alptangal@gmail.com>
2024-06-05 11:57:13 +0700

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import os
-os.system('node -v')
+os.system('''
+          npm install @dank074/discord-video-stream@latest
+npm install discord.js-selfbot-v13@latest
+          ''')
 os.system('node main.js')
