b60ab21adb7603120ae00bb08ad17d1b9476992c
alptangal <alptangal@gmail.com>
2024-06-05 15:39:52 +0700

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import os
+import os,sys
 import server
 import asyncio
 import requests
@@ -7,13 +7,16 @@ async def main():
     try:
         req=requests.get('http://localhost:8888')
         print(req.status_code)
-        print('Client closed')
-        exit()
-    except:
-        server.b()  
-        os.system('''
-                npm install @dank074/discord-video-stream@latest
-                npm install discord.js-selfbot-v13@latest
-                ''')
-        os.system('node main.js')
+        if req.status_code<400:
+            print('Client closed')
+            os._exit()
+    except Exception as error:
+        print(error)
+        if 'No connection could be made because the target machine actively refused it' in str(error) or ('req' in locals() and req.status_code>=400):
+            server.b()  
+            os.system('''
+                    npm install @dank074/discord-video-stream@latest
+                    npm install discord.js-selfbot-v13@latest
+                    ''')
+            os.system('node main.js')
 asyncio.run(main())
